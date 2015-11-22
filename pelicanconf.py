cf9be50a3c26a2a4b1e58b9cfc30c37f3e177302
junfeng_hu <grepcook@gmail.com>
2015-11-22 17:33:42 +0800

    add disqus

--- pelicanconf.py
+++ pelicanconf.py
@@ -13,6 +13,8 @@ TIMEZONE = 'Asia/Shanghai'
 
 DEFAULT_LANG = 'zh'
 
+DISQUS_SITENAME = 'junfeng'
+
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
 CATEGORY_FEED_ATOM = None
